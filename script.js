f894ee4f437650d261eb890d6634689235002c72
regiane-lima <120677112+regiane-lima@users.noreply.github.com>
2023-08-27 13:57:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cadastrarBtn = document.getElementById("cadastrarBtn");
+    const excluirBtn = document.getElementById("ExcluirBtn");
+    const voltarBtn = document.getElementById("voltarBtn");
+    const campos = document.querySelectorAll("input[type=text], input[type=email], input[type=password]");
+    const camposArray = Array.from(campos); // Converte NodeList em Array
+
+    cadastrarBtn.addEventListener("click", function() {
+        if (validarCamposPreenchidos() && validarSenhasIguais()) {
+            const novaUrl = "telaDois.html";
+            window.location.href = novaUrl;
+        } else {
+            alert("Preencha todos os campos e verifique se as senhas são iguais antes de cadastrar.");
+        }
+    });
+
+    excluirBtn.addEventListener("click", function () {
+        campos.forEach(function (campo) {
+            campo.value = "";
+        });
+    });
+
+    voltarBtn.addEventListener("click", function () {
+        window.location.href = document.referrer; // Redireciona para a página anterior usando o referenciador
+    });
+
+    function validarSenhasIguais() {
+        const senha = document.getElementById("senha").value;
+        const confirmaSenha = document.getElementById("confirmaSenha").value;
+        return senha === confirmaSenha;
+    }
+
+    function validarCamposPreenchidos() {
+        for (const campo of campos) {
+            if (campo.value.trim() === "") {
+                return false; // Pelo menos um campo está vazio
+            }
+        }
+        return true; // Todos os campos estão preenchidos
+    }
+});
